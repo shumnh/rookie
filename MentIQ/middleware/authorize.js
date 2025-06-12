49eddebfbb091c516855316ad1d6a2c9cea86336
shumnh_ <theshumanhere@gmail.com>
2025-06-13 00:00:12 +0545

    Update authorize.js

--- MentIQ/middleware/authorize.js
+++ MentIQ/middleware/authorize.js
@@ -1,8 +1,1 @@
-module.exports = (roles) => {
-  return (req, res, next) => {
-    if (!roles.includes(req.user.role)) {
-      return res.status(403).json({ msg: 'Access denied: You do not have the required role.' });
-    }
-    next();
-  };
-};
+
